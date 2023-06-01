7f0851fbea77251f06ddb1e7b0d88e741e5029d4
sangbinnam <nsb1024@naver.com>
2023-06-01 23:56:01 +0900

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Typography, Icon } from 'antd';
 import Chatbot from './Chatbot/Chatbot';
+
 const { Title } = Typography;
 
 function App() {
   return (
-    <div style={{ backgroundImage: 'url(https://www.bing.com/images/blob?bcid=S866EchhAKYF6Q)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#F8F8F8', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ backgroundImage: 'url(https://ak-d.tripcdn.com/images/1i66e2224sdhrakww89FC_W_800_0_R5_Q90.jpg?proc=source/trip)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#F8F8F8', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
         <Title level={2} style={{ color: '#FFFFFF', fontWeight: "bolder", marginBottom: 0, textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>하르방 챗봇&nbsp;<Icon type="robot" style={{ color: '#7fffd4', fontSize: '2rem' }} /></Title>
       </div>
