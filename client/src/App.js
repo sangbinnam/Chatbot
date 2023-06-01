ac583c546efc6206f11888d911aecb5837003ca6
sangbinnam <nsb1024@naver.com>
2023-06-02 00:32:26 +0900

    asdf

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ const { Title } = Typography;
 
 function App() {
   return (
-    <div style={{ backgroundImage: 'url(https://ak-d.tripcdn.com/images/1i66e2224sdhrakww89FC_W_800_0_R5_Q90.jpg?proc=source/trip)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#F8F8F8', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ backgroundImage: 'url()', backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: '#F8F8F8', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
         <Title level={2} style={{ color: '#FFFFFF', fontWeight: "bolder", marginBottom: 0, textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>하르방 챗봇&nbsp;<Icon type="robot" style={{ color: '#7fffd4', fontSize: '2rem' }} /></Title>
       </div>
--- server/config/secret.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtsecret: "jwt_secret_key_9312",
-    host: "3.37.2.89"
-}
